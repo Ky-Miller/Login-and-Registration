7c82c80156769a9a5e331a7de6fb1bdaa01a1c57
Ky-Miller <kychr.miller@gmail.com>
2023-06-23 15:15:09 -0700

    1st commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask_app import app
+from flask_app.controllers import login_rename
+
+# =========================================================
+# REMEMBER TO IMPORT CONTROLLERS INTO YOUR SERVER FILE!
+# =========================================================
+
+# from flask_app.controllers import
+
+# RUN pipenv install PyMySQL flask flask-bcrypt
+# REMEMBER TO SAVE YOUR .mwb FILE TO THE FOLDER!
+
+if __name__ == "__main__":
+    app.run(debug=True)
